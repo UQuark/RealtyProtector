b7a3d38e0e22e52d9b009069cb3198d77ea58a4a
Artemii Bigdan <artem@bigdan.in>
2020-04-24 19:00:11 +0300

    Setup

---
+++ src/main/java/me/uquark/realtyprotector/RealtyProtector.java
@@ -0,0 +1,15 @@
+package me.uquark.realtyprotector;
+
+import me.uquark.quarkcore.base.AbstractMod;
+import net.fabricmc.api.ModInitializer;
+
+public class RealtyProtector extends AbstractMod implements ModInitializer {
+    public RealtyProtector() {
+        super("realtyprotector");
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
